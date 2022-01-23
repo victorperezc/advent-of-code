f07d36996032e8e93d3f87ea6b301b43f34371a0
Victor Perez <victor.perez.9889@gmail.com>
2022-01-23 16:39:16 +0000

    Advent of Code 2021 Day 1

---
+++ 2021/day1/src/main.rs
@@ -0,0 +1,20 @@
+use std::{fs,env};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    let depths : Vec<&str> = contents.split("\n").collect();
+    let mut count : u32 = 0;
+    for i in 1..depths.len() {
+        if depths[i-1] < depths[i] {
+            count = count + 1;
+        }
+    }
+
+    println!("How many measurements are larger than the previous measurement?");
+    println!("There are {} measurements larger than the previous", count);
+
+    let windows : Vec<&str> = Vec::new();
+    
+}
